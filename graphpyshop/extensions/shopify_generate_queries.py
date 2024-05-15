df4539335c5d754454db64eab57749a1c8e44f9b
salomartin <martin@yummy.eu>
2024-05-15 15:15:41 +0100

    Improves graphql query generator

--- graphpyshop/extensions/shopify_generate_queries.py
+++ graphpyshop/extensions/shopify_generate_queries.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import orjson
 from graphql import build_client_schema, print_schema, parse, print_ast
 from graphql.language import ast as graphql_ast
@@ -12,7 +13,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 config_dict = get_config_dict()
 settings = get_client_settings(config_dict)
-
+"""
 if settings.schema_path:
     schema = get_graphql_schema_from_path(settings.schema_path)
 else:
@@ -21,7 +22,8 @@ else:
         headers=settings.remote_schema_headers,
         verify_ssl=settings.remote_schema_verify_ssl,
     )
-#schema = get_graphql_schema_from_path('/Users/salomartin/Development/yummy/GraphPyShop/graphpyshop/extensions/schema.graphql')
+"""
+schema = get_graphql_schema_from_path('/Users/salomartin/Development/yummy/GraphPyShop/graphpyshop/extensions/schema.graphql')
 
 # Step 3: Convert to AST
 sdl = print_schema(schema)
@@ -29,7 +31,7 @@ with open('/Users/salomartin/Development/yummy/GraphPyShop/graphpyshop/extension
     schema_file.write(sdl)
 ast = parse(sdl)
 
-max_depth = 4
+max_depth = 2
 
 # Step 4: Inspect the AST and generate queries
 output_dir = '/Users/salomartin/Development/yummy/GraphPyShop/graphpyshop/extensions/generated_queries'
@@ -71,13 +73,17 @@ def find_ultimate_object(type_name: str) -> str:
 
 list_returning_queries = {}
 
+def returns_a_list(field: graphql_ast.FieldDefinitionNode) -> bool:
+    field_type_name = get_field_type_name(field.type)
+    return field_type_name.endswith("Connection") or isinstance(field.type, graphql_ast.ListTypeNode)
+
 for definition in ast.definitions:
     if isinstance(definition, graphql_ast.ObjectTypeDefinitionNode) and definition.name.value == "QueryRoot":
         for field in definition.fields:
             if not is_deprecated(field):
                 field_type_name = get_field_type_name(field.type)
                 ultimate_object = find_ultimate_object(field_type_name)
-                if field_type_name.endswith("Connection") or isinstance(field.type, graphql_ast.ListTypeNode):
+                if returns_a_list(field):
                     list_returning_queries[field.name.value] = ultimate_object
 
 reversed_queries = {}
@@ -90,110 +96,81 @@ for key, value in list_returning_queries.items():
     else:
         reversed_queries[value] = key
 #TODO: Avoid implementing faulty one
-#list_returning_queries = reversed_queries
+list_returning_queries = reversed_queries
 
-def generate_query_ast(field: graphql_ast.FieldDefinitionNode, visited_types: dict[str, int], depth: int = 0, max_depth: int = max_depth, connection_count: int = 0, max_connections: int = 10) -> graphql_ast.SelectionSetNode:
-    logging.info(f"Generating query AST for field: {field.name.value}, depth: {depth}, connection_count: {connection_count}")
+scalar_types = {definition.name.value for definition in ast.definitions if isinstance(definition, graphql_ast.ScalarTypeDefinitionNode)}
+enum_types = {definition.name.value for definition in ast.definitions if isinstance(definition, graphql_ast.EnumTypeDefinitionNode)}
+
+def is_core_type(type_name: str) -> bool:
+    core_types = {"String", "Int", "Float", "Boolean", "ID", "DateTime", "UnsignedInt64"}
+    return type_name in core_types or type_name in scalar_types or type_name in enum_types
+
+def generate_query_ast(query_name: str, field: graphql_ast.FieldDefinitionNode, visited_types: dict[str, int], depth: int = 0, max_depth: int = max_depth, parent: Optional[graphql_ast.FieldDefinitionNode] = None, path: str = "") -> graphql_ast.SelectionSetNode:
+    current_path = f"{path} > {field.name.value}" if path else field.name.value
+    field_type_name = get_field_type_name(field.type)
+    logging.info(f"[{query_name}][{current_path}][depth: {depth}] Generating query AST for field: {current_path}, depth: {depth}")
     
-    if depth > max_depth or connection_count >= max_connections:
-        logging.info(f"Max depth or max connections reached. Returning empty selection set.")
+    # Are there any fields with only nodes without edges?
+    if field.name.value == 'nodes':
+        logging.info(f"[{query_name}][{current_path}][depth: {depth}] Skipping all nodes fields as we traverse only edges > node")
+        return graphql_ast.SelectionSetNode(selections=[])
+    
+    if depth > max_depth:
+        logging.info(f"[{query_name}][{current_path}][depth: {depth}] Max depth reached. Returning empty selection set.")
         return graphql_ast.SelectionSetNode(selections=[])
 
-    field_type = get_field_type(field.type)
-    selections = []
+    if is_deprecated(field):
+        logging.info(f"[{query_name}][{current_path}][depth: {depth}] Field {field.name.value} is deprecated. Skipping.")
+        return graphql_ast.SelectionSetNode(selections=[])
 
-    if isinstance(field_type, graphql_ast.NamedTypeNode):
-        field_type_name = field_type.name.value
-        logging.info(f"Field type is NamedTypeNode with name: {field_type_name}")
-        
-        if field_type_name in visited_types and visited_types[field_type_name] > 0:
-            logging.info(f"Field type {field_type_name} already visited. Adding 'id' field to selections.")
-            selections.append(graphql_ast.FieldNode(name=graphql_ast.NameNode(value="id")))
-        else:
-            visited_types[field_type_name] = visited_types.get(field_type_name, 0) + 1
-            logging.info(f"Visiting field type {field_type_name}. Updated visited_types: {visited_types}")
-            
-            for definition in ast.definitions:
-                if isinstance(definition, graphql_ast.ObjectTypeDefinitionNode) and definition.name.value == field_type_name:
-                    logging.info(f"Processing ObjectTypeDefinitionNode: {definition.name.value}")
+    selections: List[graphql_ast.FieldNode] = []
+    # Check if the field has children (fields)
+    has_children = False
+    for definition in ast.definitions:
+        if isinstance(definition, graphql_ast.ObjectTypeDefinitionNode) and definition.name.value == get_field_type_name(field.type):
+            has_children = True
+            logging.info(f"[{query_name}][{current_path}][depth: {depth}] Processing ObjectTypeDefinitionNode: {definition.name.value}")
+
+            for sub_field in definition.fields:
+                if not is_deprecated(sub_field):
+                    # Do not count depth if the field is named 'edges' or 'node'
+                    new_depth = depth if sub_field.name.value in {"edges", "node", "pageInfo"} else depth + 1
+
+                    sub_query = generate_query_ast(query_name, sub_field, visited_types, new_depth, max_depth, field, current_path)
+
+                    # Check if the sub_query has any selections and would exceed max depth
+                    if len(sub_query.selections) == 0 and not is_core_type(get_field_type_name(sub_field.type)):
+                        logging.warning(f"[{query_name}][{current_path}][depth: {depth}] Field {sub_field.name.value} should have children but doesn't. Returning empty selection set.")
+                        continue
+
+                    if new_depth > max_depth:
+                        logging.warning(f"[{query_name}][{current_path}][depth: {depth}] Max depth reached for field {sub_field.name.value}. Skipping this field.")
+                        continue
                     
-                    for sub_field in definition.fields:
-                        if not is_deprecated(sub_field):
-                            sub_field_type = get_field_type(sub_field.type)
-                            logging.info(f"Processing sub-field: {sub_field.name.value}, type: {sub_field_type}")
-                            
-                            if isinstance(sub_field_type, graphql_ast.NamedTypeNode):
-                                sub_field_type_name = sub_field_type.name.value
-                                logging.info(f"Sub-field type is NamedTypeNode with name: {sub_field_type_name}")
-                                
-                                if 'Connection' in sub_field_type_name:
-                                    if connection_count < max_connections:
-                                        logging.info(f"Sub-field type {sub_field_type_name} is a Connection. Generating sub-query.")
-                                        sub_query = generate_query_ast(sub_field, visited_types, depth + 1, max_depth, connection_count + 1, max_connections)
-                                        # Check if the ultimate object of the connection is in list_returning_queries
-                                        ultimate_object = list_returning_queries.get(sub_field_type_name)
-                                        if ultimate_object and ultimate_object in list_returning_queries:
-                                            logging.info(f"Ultimate object {ultimate_object} is in list_returning_queries. Only adding 'id' field.")
-                                            selections.append(graphql_ast.FieldNode(
-                                                name=graphql_ast.NameNode(value=sub_field.name.value),
-                                                selection_set=graphql_ast.SelectionSetNode(selections=[
-                                                    graphql_ast.FieldNode(
-                                                        name=graphql_ast.NameNode(value="edges"),
-                                                        selection_set=graphql_ast.SelectionSetNode(selections=[
-                                                            graphql_ast.FieldNode(
-                                                                name=graphql_ast.NameNode(value="node"),
-                                                                selection_set=graphql_ast.SelectionSetNode(selections=[
-                                                                    graphql_ast.FieldNode(name=graphql_ast.NameNode(value="id"))
-                                                                ])
-                                                            )
-                                                        ])
-                                                    )
-                                                ])
-                                            ))
-                                        else:
-                                            selections.append(graphql_ast.FieldNode(
-                                                name=graphql_ast.NameNode(value=sub_field.name.value),
-                                                selection_set=graphql_ast.SelectionSetNode(selections=[
-                                                    graphql_ast.FieldNode(
-                                                        name=graphql_ast.NameNode(value="edges"),
-                                                        selection_set=graphql_ast.SelectionSetNode(selections=[
-                                                            graphql_ast.FieldNode(
-                                                                name=graphql_ast.NameNode(value="node"),
-                                                                selection_set=sub_query
-                                                            )
-                                                        ])
-                                                    )
-                                                ])
-                                            ))
-                                else:
-                                    if sub_field_type_name in list_returning_queries:
-                                        logging.info(f"Sub-field type {sub_field_type_name} is in list_returning_queries. Adding 'id' field to selections.")
-                                        selections.append(graphql_ast.FieldNode(name=graphql_ast.NameNode(value="id")))
-                                    elif sub_field_type_name not in visited_types or visited_types[sub_field_type_name] == 0:
-                                        logging.info(f"Sub-field type {sub_field_type_name} is not a Connection. Generating sub-query.")
-                                        sub_query = generate_query_ast(sub_field, visited_types, depth + 1, max_depth, connection_count, max_connections)
-                                        selections.append(graphql_ast.FieldNode(
-                                            name=graphql_ast.NameNode(value=sub_field.name.value),
-                                            selection_set=sub_query
-                                        ))
-                            else:
-                                logging.info(f"Sub-field {sub_field.name.value} is not a NamedTypeNode. Adding to selections.")
-                                selections.append(graphql_ast.FieldNode(name=graphql_ast.NameNode(value=sub_field.name.value)))
-            visited_types[field_type_name] -= 1
-            logging.info(f"Finished processing field type {field_type_name}. Updated visited_types: {visited_types}")
-    else:
-        logging.info(f"Field type is not a NamedTypeNode. Adding field {field.name.value} to selections.")
-        selections.append(graphql_ast.FieldNode(name=graphql_ast.NameNode(value=field.name.value)))
+                    subfield_type_name = get_field_type_name(sub_field.type)
+                    if field.name.value == "node" and field_type_name in list_returning_queries and depth != 0 and subfield_type_name != "ID":
+                        logging.info(f"[{query_name}][{current_path}][depth: {depth}] It's a list returning field and type is not id, returning empty set")
+                        continue
+                    
+                    selections.append(graphql_ast.FieldNode(
+                        name=graphql_ast.NameNode(value=sub_field.name.value),
+                        selection_set=sub_query
+                    ))
+            break
 
-    # Remove 'node' and 'pageInfo' if 'edges' is present
+    if not has_children:
+        logging.info(f"[{query_name}][{current_path}][depth: {depth}] Field {field.name.value} has no children. Skipping nested selection.")
+        return graphql_ast.SelectionSetNode(selections=[])
+
+    # Remove 'node', 'nodes', and 'pageInfo' if 'edges' is present
     if any(selection.name.value == "edges" for selection in selections):
-        selections = [selection for selection in selections if selection.name.value not in {"nodes", "pageInfo"}]
+        selections = [selection for selection in selections if selection.name.value not in {"nodes"}]
 
-    logging.info(f"Returning selection set with {len(selections)} selections.")
+    logging.info(f"[{query_name}][{current_path}][depth: {depth}] Returning selection set with {len(selections)} selections.")
     return graphql_ast.SelectionSetNode(selections=selections)
 
-def generate_query_with_variables_ast(field: graphql_ast.FieldDefinitionNode, visited_types: dict[str, int], depth: int = 0, max_depth: int = max_depth, connection_count: int = 0, max_connections: int = 10) -> graphql_ast.OperationDefinitionNode:
-    query_fields = generate_query_ast(field, visited_types, depth, max_depth, connection_count, max_connections)
+def generate_query_with_variables_ast(query_name: str, field: graphql_ast.FieldDefinitionNode, visited_types: dict[str, int], depth: int = 0, max_depth: int = max_depth) -> graphql_ast.OperationDefinitionNode:
+    query_fields = generate_query_ast(query_name, field, visited_types, depth, max_depth)
     variable_definitions = []
     arguments = []
 
@@ -238,7 +215,7 @@ for definition in ast.definitions:
                 query_name = field.name.value
                 if query_name == "customers":
                     visited_types: dict[str, int] = {}
-                    query_ast = generate_query_with_variables_ast(field, visited_types)
+                    query_ast = generate_query_with_variables_ast(query_name, field, visited_types)
                     query_str = print_ast(query_ast)
 
                     # Save the query to a file
