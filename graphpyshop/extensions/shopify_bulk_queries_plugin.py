c93e6c3199f07a5804d5bcb2af404c77e2940920
salomartin <martin@yummy.eu>
2024-05-28 18:45:10 +0200

    Update generation and fix build

--- graphpyshop/extensions/shopify_bulk_queries_plugin.py
+++ graphpyshop/extensions/shopify_bulk_queries_plugin.py
@@ -1,13 +1,12 @@
 import ast
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Tuple
 
 from ariadne_codegen.codegen import (
     generate_import_from,
 )
 from ariadne_codegen.plugins.base import Plugin
 from graphql import GraphQLSchema
-
 
 class ShopifyBulkQueriesPlugin(Plugin):
     _ignore_args: set[str] = set()
@@ -27,8 +26,9 @@ class ShopifyBulkQueriesPlugin(Plugin):
                 new_methods = self._enhance_class_with_bulk_methods(node, module)
                 node.body.extend(new_methods)  # Append new methods after collecting all
 
-        ast.fix_missing_locations(module)  # Fix locations for the entire module
         self._add_necessary_imports(module)
+        self._flush_pending_imports(module)  # Ensure all pending imports are added
+        ast.fix_missing_locations(module)
         return module
 
     def _collect_imports(self, module: ast.Module):
@@ -65,52 +65,172 @@ class ShopifyBulkQueriesPlugin(Plugin):
         self, class_def: ast.ClassDef, module: ast.Module
     ):
         new_methods = []
+        existing_method_names = {method.name for method in class_def.body if isinstance(method, ast.FunctionDef)}
+
         for method in class_def.body:
             if isinstance(method, ast.AsyncFunctionDef):
-                logging.info(f"Found async function: {method.name}")
+                #logging.info(f"Found async function: {method.name}")
+                bulk_method_name = f"bq_{method.name}"
+                if bulk_method_name in existing_method_names:
+                    logging.info(f"Skipping bulk method creation for already existing method: {bulk_method_name}")
+                    continue
+                existing_method_names.add(bulk_method_name)
                 # Skip if the method is already a bulk method
                 if method.name.startswith("bq_"):
                     logging.info(
                         f"Skipping bulk method creation for already enhanced method: {method.name}"
                     )
                     continue
-                # Check if the return type is Optional
-                if self._is_optional_return_type(method.returns):
+                if method.returns is None:
                     logging.info(
-                        f"Skipping bulk method creation for: {method.name} due to Optional return type."
+                        f"Skipping bulk method creation for: {method.name} due to it not returning anything"
                     )
                     continue
+
+                return_type = self._is_list_return_type(method.returns, module)
+                if not return_type:
+                    logging.info(
+                        f"Skipping bulk method creation for: {method.name} due to it not returning a list"
+                    )
+                    continue
+
                 gql_var_name = f"{method.name.upper()}_GQL"
-                bulk_method = self._create_bulk_method(method, gql_var_name, module)
+                bulk_method = self._create_bulk_method(
+                    method, gql_var_name, module, return_type
+                )
                 if bulk_method:
                     new_methods.append(bulk_method)
                     logging.info(f"Bulk method created: {bulk_method.name}")
         return new_methods
 
-    def _is_optional_return_type(self, return_type):
-        if isinstance(return_type, ast.Subscript) and isinstance(
-            return_type.value, ast.Name
-        ):
-            return return_type.value.id == "Optional"
-        return False
+    def _is_list_return_type(
+        self, return_type: ast.expr, module: ast.Module
+    ) -> Optional[ast.expr]:
+        if isinstance(return_type, ast.Name):
+            class_name = return_type.id
+            result = self._get_class_ast(class_name, module)
+            if result:
+                class_ast, class_module_name = result
+                class_def = self._find_class_in_ast(class_name, class_ast)
+                if class_def:
+                    for node in ast.walk(class_def):
+                        if isinstance(node, ast.AnnAssign) and isinstance(
+                            node.annotation, ast.Subscript
+                        ) and isinstance(node.target, ast.Name) and node.target.id=="edges" and isinstance(node.annotation.value, ast.Name) and node.annotation.value.id == "List":
+                            if isinstance(node.annotation.slice, ast.Constant) and node.annotation.slice.value:
+                                node_class_def = self._find_class_in_ast(node.annotation.slice.value, class_ast)
+                                if node_class_def:
+                                    for class_node in ast.walk(node_class_def):
+                                        if isinstance(class_node, ast.AnnAssign) and isinstance(class_node.target, ast.Name) and class_node.target.id=="node":
+                                        #if isinstance(class_node, ast.AnnAssign) and isinstance(class_node.target, ast.Name) and class_node.target.id == 'node':
+                                            # Add import to module body
+                                            self._add_import_to_module(class_node.annotation, module, "."+class_module_name)
+                                            return class_node.annotation
+        return None
+
+    def _get_class_ast(
+        self, class_name: str, module: ast.Module
+    ) -> Optional[Tuple[ast.Module, str]]:
+        for node in ast.walk(module):
+            if isinstance(node, ast.ImportFrom):
+                for alias in node.names:
+                    if alias.name == class_name and node.module:
+                        absolute_module = f"graphpyshop.client.{node.module}"
+                        try:
+                            # Get the file path of the module
+                            module_file_path = absolute_module.replace(".", "/") + ".py"
+                            with open(module_file_path, "r") as file:
+                                class_source = file.read()
+                            return ast.parse(class_source), node.module
+                        except (FileNotFoundError, OSError) as e:
+                            logging.warning(
+                                f"Could not read {class_name} from {absolute_module}: {e}"
+                            )
+                            return None
+        return None
+    def _find_class_in_ast(
+        self, class_name: str, class_ast: ast.Module
+    ) -> Optional[ast.ClassDef]:
+        for class_node in ast.walk(class_ast):
+            if isinstance(class_node, ast.ClassDef) and class_node.name == class_name:
+                return class_node
+        return None
+    
+    def _add_import_to_module(self, class_name: ast.expr, module: ast.Module, module_name: str):
+        if isinstance(class_name, ast.Subscript):
+            # Handle subscript case (e.g., List[str])
+            if isinstance(class_name.slice, ast.Constant):
+                self._add_import(name=class_name.slice.value, module=module, module_name=module_name)
+            elif isinstance(class_name.slice, ast.Tuple):
+                for elt in class_name.slice.elts:
+                    if isinstance(elt, ast.Constant):
+                        self._add_import(name=elt.value, module=module, module_name=module_name)
+        elif isinstance(class_name, ast.Constant):
+            # Handle simple constant case (e.g., str)
+            self._add_import(name=class_name.value, module=module, module_name=module_name)
+        else:
+            logging.error(f"Unsupported type for class_name: {type(class_name).__name__}")
+
+    def _add_import(self, name: str, module: ast.Module, module_name: str):
+        # Collect all names to be imported from the same module
+        if not hasattr(self, 'pending_imports'):
+            self.pending_imports = {}
+
+        if module_name not in self.pending_imports:
+            self.pending_imports[module_name] = set()
+
+        self.pending_imports[module_name].add(name)
+
+    def _flush_pending_imports(self, module: ast.Module):
+        # Generate import statements for all collected names grouped by module
+        for module_name, names in self.pending_imports.items():
+            import_stmt = ast.ImportFrom(
+                module=module_name,
+                names=[ast.alias(name=name, asname=None) for name in names],
+                level=0
+            )
+            module.body.insert(0, import_stmt)
+            logging.info(f"Import for {', '.join(names)} from {module_name} added.")
+            self.pending_imports = {}  # Reset after flushing
+
 
     def _create_bulk_method(
-        self, method_def: ast.AsyncFunctionDef, gql_var_name: str, module: ast.Module
+        self,
+        method_def: ast.AsyncFunctionDef,
+        gql_var_name: str,
+        module: ast.Module,
+        return_type: ast.expr,
     ) -> ast.AsyncFunctionDef:
         logging.info(f"Creating bulk method for: {method_def.name}")
         bulk_method_def = ast.AsyncFunctionDef(
             name=f"bq_{method_def.name}",
             args=method_def.args,  # Preserve original args
-            body=self._generate_bulk_method_body(method_def, gql_var_name, module),
+            body=self._generate_bulk_method_body(
+                method_def, gql_var_name, module, return_type
+            ),
             decorator_list=[],
-            returns=self._process_return_type(
-                method_def.returns, method_def
-            ),  # Process while referencing the original method for positional data
+            returns=ast.Subscript(
+                value=ast.Name(id="AsyncGenerator", ctx=ast.Load()),
+                slice=ast.Index(
+                    value=ast.Tuple(
+                        elts=[
+                            return_type,
+                            ast.Name(id="None", ctx=ast.Load()),
+                        ],
+                        ctx=ast.Load(),
+                    )
+                ),
+                ctx=ast.Load(),
+            ),
         )
         return bulk_method_def
 
     def _generate_bulk_method_body(
-        self, method_def: ast.AsyncFunctionDef, gql_var_name: str, module: ast.Module
+        self,
+        method_def: ast.AsyncFunctionDef,
+        gql_var_name: str,
+        module: ast.Module,
+        return_type: ast.expr,
     ):
         # Generate the variables assignment dynamically based on the method definition, excluding 'self'
         variables_assignment = ast.AnnAssign(
@@ -128,7 +248,7 @@ class ShopifyBulkQueriesPlugin(Plugin):
             ),
             value=ast.Dict(
                 keys=[
-                    ast.Str(s=arg.arg)
+                    ast.Constant(value=arg.arg)
                     for arg in method_def.args.args
                     if arg.arg not in self._ignore_args_full
                 ],
@@ -140,31 +260,6 @@ class ShopifyBulkQueriesPlugin(Plugin):
             ),
             simple=1,
         )
-
-        # Determine the return type and corresponding edges node type
-        return_type_id = self._get_return_type_id(method_def.returns)
-        edges_node_type = f"{return_type_id}EdgesNode"
-
-        # Use the same module path as the original return type for edges node type
-        original_module_path = self.imported_types.get(
-            return_type_id, f".{return_type_id.lower()}"
-        )
-
-        # Ensure the original return type and its edges node type are imported from the same module
-        if return_type_id not in self.imported_types:
-            module.body.insert(
-                0, generate_import_from([return_type_id], original_module_path)
-            )
-            self.imported_types[return_type_id] = original_module_path
-
-        if edges_node_type not in self.imported_types:
-            edges_node_module_path = (
-                original_module_path  # Same module path as the original return type
-            )
-            module.body.insert(
-                0, generate_import_from([edges_node_type], edges_node_module_path)
-            )
-            self.imported_types[edges_node_type] = edges_node_module_path
 
         # Construct the call for the async for loop
         call_expression = ast.Call(
@@ -188,7 +283,6 @@ class ShopifyBulkQueriesPlugin(Plugin):
                 ast.Name(id="variables", ctx=ast.Load()),
                 ast.Name(id="BulkOperationStatus", ctx=ast.Load()),
                 ast.Name(id="BulkOperationNodeBulkOperation", ctx=ast.Load()),
-                ast.Name(id=edges_node_type, ctx=ast.Load()),
             ],
             keywords=[],
         )
@@ -202,31 +296,3 @@ class ShopifyBulkQueriesPlugin(Plugin):
         )
 
         return [variables_assignment, async_for_loop]
-
-    def _get_return_type_id(self, node):
-        if isinstance(node, ast.Name):
-            return node.id
-        elif isinstance(node, ast.Subscript):
-            return self._get_return_type_id(
-                node.value
-            )  # Recursive call to handle nested types
-        return "Unknown"
-
-    def _process_return_type(self, return_type, source_node):
-        # Return type adjustment for AsyncGenerator
-        return ast.Subscript(
-            value=ast.Name(id="AsyncGenerator", ctx=ast.Load()),
-            slice=ast.Index(
-                value=ast.Tuple(
-                    elts=[
-                        ast.Name(
-                            id=f"{self._get_return_type_id(return_type)}EdgesNode",
-                            ctx=ast.Load(),
-                        ),
-                        ast.Name(id="None", ctx=ast.Load()),
-                    ],
-                    ctx=ast.Load(),
-                )
-            ),
-            ctx=ast.Load(),
-        )
